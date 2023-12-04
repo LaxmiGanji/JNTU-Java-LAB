ae3dc0019ed4c100b72f0db4fee9b920bbb13d14
Laxmi Ganji <117654059+LaxmiGanji@users.noreply.github.com>
2023-12-04 12:24:08 +0530

    Update operation.java

--- operation.java
+++ operation.java
@@ -1,3 +1,31 @@
+//without using applet
+import java.util.Scanner;
+
+public class ArithmeticDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        float quotient = (float) num1 / num2;
+        int remainder = num1 % num2;
+
+        System.out.println("The sum of the two numbers is: " + sum);
+        System.out.println("The difference of the two numbers is: " + difference);
+        System.out.println("The product of the two numbers is: " + product);
+        System.out.println("The quotient of the two numbers is: " + quotient);
+        System.out.println("The remainder of the two numbers is: " + remainder);
+    }
+}
+
+//with using applet
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
